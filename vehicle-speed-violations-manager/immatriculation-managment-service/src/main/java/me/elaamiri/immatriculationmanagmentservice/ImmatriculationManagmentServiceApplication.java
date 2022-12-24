3ba7fe00abeb0c4ec95340d4acac9bcd39da973e
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-24 15:20:51 +0100

    init immatriculation-managment-service

---
+++ vehicle-speed-violations-manager/immatriculation-managment-service/src/main/java/me/elaamiri/immatriculationmanagmentservice/ImmatriculationManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package me.elaamiri.immatriculationmanagmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmatriculationManagmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmatriculationManagmentServiceApplication.class, args);
+    }
+
+}
