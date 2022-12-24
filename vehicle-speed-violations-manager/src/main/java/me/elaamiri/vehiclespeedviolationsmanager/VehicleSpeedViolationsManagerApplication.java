97ff2a765227b7441de875ee8548c7ae299e0479
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-24 09:41:49 +0100

    init project

---
+++ vehicle-speed-violations-manager/src/main/java/me/elaamiri/vehiclespeedviolationsmanager/VehicleSpeedViolationsManagerApplication.java
@@ -0,0 +1,13 @@
+package me.elaamiri.vehiclespeedviolationsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleSpeedViolationsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleSpeedViolationsManagerApplication.class, args);
+	}
+
+}
