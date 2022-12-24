167b04510b15aa09cc449df3314801f1a60f7537
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-24 21:38:19 +0100

    init infractions-managment-service

---
+++ vehicle-speed-violations-manager/infractions-management-service/src/main/java/me/elaamiri/infractionsmanagementservice/InfractionsManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package me.elaamiri.infractionsmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfractionsManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfractionsManagementServiceApplication.class, args);
+    }
+
+}
---
+++ vehicle-speed-violations-manager/infractions-management-service/src/test/java/me/elaamiri/infractionsmanagementservice/InfractionsManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.infractionsmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfractionsManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
