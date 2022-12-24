8f792a2d5e3da3d782b85e91c4276b4add1f297e
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-24 10:38:46 +0100

    init common-api

---
+++ vehicle-speed-violations-manager/radar-management/src/main/java/me/elaamiri/radarmanagement/RadarManagementApplication.java
@@ -0,0 +1,13 @@
+package me.elaamiri.radarmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadarManagementApplication.class, args);
+    }
+
+}
---
+++ vehicle-speed-violations-manager/radar-management/src/test/java/me/elaamiri/radarmanagement/RadarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.radarmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
