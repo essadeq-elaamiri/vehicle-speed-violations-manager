d7addbfb545d8feca75d9b59bff0434462ea4c78
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-25 14:49:49 +0100

    init gateway-service

--- vehicle-speed-violations-manager/immatriculation-managment-service/src/main/java/me/elaamiri/immatriculationmanagmentservice/ImmatriculationManagmentServiceApplication.java
+++ vehicle-speed-violations-manager/immatriculation-managment-service/src/main/java/me/elaamiri/immatriculationmanagmentservice/ImmatriculationManagmentServiceApplication.java
@@ -5,11 +5,10 @@ import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.SimpleCommandBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ImmatriculationManagmentServiceApplication {
 
     public static void main(String[] args) {
