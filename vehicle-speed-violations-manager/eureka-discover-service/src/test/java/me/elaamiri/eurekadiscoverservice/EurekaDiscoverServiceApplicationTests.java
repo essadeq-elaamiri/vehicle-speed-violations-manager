161ddd237357bdc38bf9b440feea9e7b50f4c3a3
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-25 03:20:15 +0100

    init Eureka Discovery Service

---
+++ vehicle-speed-violations-manager/eureka-discover-service/src/test/java/me/elaamiri/eurekadiscoverservice/EurekaDiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elaamiri.eurekadiscoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
