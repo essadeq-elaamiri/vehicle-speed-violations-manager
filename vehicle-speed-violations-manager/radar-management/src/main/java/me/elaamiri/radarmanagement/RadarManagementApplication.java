b071274450f54beedbc67213746021659718e56d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-12-25 16:33:02 +0100

    gateway + dicovery DONE, tested

--- vehicle-speed-violations-manager/radar-management/src/main/java/me/elaamiri/radarmanagement/RadarManagementApplication.java
+++ vehicle-speed-violations-manager/radar-management/src/main/java/me/elaamiri/radarmanagement/RadarManagementApplication.java
@@ -5,9 +5,11 @@ import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.SimpleCommandBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RadarManagementApplication {
 
     public static void main(String[] args) {
